ad7d0be7c0a9410453082409a17dd3f2094c6e3e
Only Huân <71635685+huannd0101@users.noreply.github.com>
2021-10-19 00:08:01 +0700

    ...

---
+++ BTVN_B1/src/Bai2/RunMain.java
@@ -0,0 +1,28 @@
+
+package Bai2;
+import java.util.Scanner;
+
+public class RunMain {
+    public static void main(String[] args){
+        
+        System.out.println("*       *     *     * * * *   * * * *  *     *");
+        System.out.println("|       |    / \\    |      *  |      *  \\   /");
+        System.out.println("* ----- *   *---*   * * * *   * * * *     *");
+        System.out.println("|       |  /     \\  |         |           |");
+        System.out.println("*       * *       * *         *           *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*    *  ******  *       *       *");
+        System.out.println("| \\  |  |        \\     / \\     /");
+        System.out.println("*  * *  ******    *   *   *   *");
+        System.out.println("|   \\|  |          \\ /     \\ /");
+        System.out.println("*    *  ******      *       *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*   *  ******      *      * * * *");
+        System.out.println(" \\ /   |          / \\     |       *");
+        System.out.println("  *    ******    *---*    * * * * ");
+        System.out.println("  |    |        /     \\   |    \\");
+        System.out.println("  *    ******  *       *  *     *");
+    }
+}
